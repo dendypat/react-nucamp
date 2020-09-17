824d546a42ff409e20190d592d72fb8488685588
Patience Dendy <dendypat@gmail.com>
2020-09-17 19:28:41 -0400

    Breadcrumbs

--- nucampsite/src/index.js
+++ nucampsite/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import 'typeface-lobster';
 import 'typeface-open-sans';
 import 'font-awesome/css/font-awesome.css';
